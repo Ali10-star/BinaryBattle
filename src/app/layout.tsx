dc0af23ffd7cba7e3215a1e2ed395880f3d9fdbb
Ali10-star <Alosh981@hotmail.com>
2024-11-04 01:33:56 +0400

    Added Radix-UI themes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import "@radix-ui/themes/styles.css";
+
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
